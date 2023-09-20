8fdd12d1020d901cf8ffa02a33b81e8e2923a619
Sumit Sardana <sardana.sumit08@gmail.com>
2023-09-21 02:13:19 +0530

    - dependency update

---
+++ plugin/src/main/java/com/sumit/trino/ExampleNullFunction.java
@@ -0,0 +1,18 @@
+package com.sumit.trino;
+
+import io.airlift.slice.Slice;
+import io.trino.spi.function.Description;
+import io.trino.spi.function.ScalarFunction;
+import io.trino.spi.function.SqlNullable;
+import io.trino.spi.function.SqlType;
+import io.trino.spi.type.StandardTypes;
+
+public class ExampleNullFunction {
+    @ScalarFunction(value = "is_null_poc", deterministic = true)
+    @Description("Returns TRUE if the argument is NULL")
+    @SqlType(StandardTypes.BOOLEAN)
+    public static boolean isNull(
+            @SqlNullable @SqlType(StandardTypes.VARCHAR) Slice string ) {
+        return string == null;
+    }
+}
